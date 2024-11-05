ef88768be6bbf0fce4207b805c078e06d5f35b68
rbeau12 <107488237+rbeau12@users.noreply.github.com>
2024-11-05 23:30:29 +0000

    Working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def backdoor
+    
+    render({:template=>"item_templates/backdoor"})
+  end
+  def post
+    url=params.fetch(:link)
+    description=params.fetch(:description)
+    image=params.fetch(:image)
+    c=Item.new
+    c.link_url=url
+    c.link_description=description
+    c.thumbnail_url=image
+    c.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor",{:controller=>"items",:action=>"backdoor"})
+  post("/post",{:controller=>"items",:action=>"post"})
 end
